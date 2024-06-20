482d2bc3314a19a10bbd0f302bc02315648858e0
AZIZBEKJURAEV <ajuraev98@mail.ru>
2024-06-21 01:41:38 +0500

    not completed

---
+++ index.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", function () {
+  const slideContainer = document.querySelector(".carousel");
+  const slidesWrapper = document.querySelector(".carousel-slides");
+  let slides = document.querySelectorAll(".carousel-slide");
+  let index = 0;
+  const interval = 1500;
+  let moveDistance = 0;
+  const paddingRight = 50;
+  let lastSlideIndex = slides.length - 1;
+  let firstClone;
+
+  const startSlide = (index) => {
+    this.setInterval(() => {
+      moveDistance = moveDistance + slides[index].clientWidth + paddingRight;
+      slidesWrapper.style.transform = `translateX(${-moveDistance}px)`;
+      slidesWrapper.style.transition = "1s";
+
+      firstClone = slides[index].cloneNode(true);
+      firstClone.id = `first-clone-${index}`;
+      slidesWrapper.append(firstClone);
+      index++;
+    }, interval);
+  };
+  startSlide(index);
+});
