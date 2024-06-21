efea016a5bd6215ce5b32db9e108f15581859b03
AZIZBEKJURAEV <ajuraev98@mail.ru>
2024-06-22 00:42:56 +0500

    fixing ui

--- index.js
+++ index.js
@@ -22,3 +22,17 @@ window.addEventListener("load", function () {
   };
   startSlide(index);
 });
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop,
+        behavior: "smooth", // Smooth scroll
+      });
+    }
+  });
+});
